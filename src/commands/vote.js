e525de69e00eb97b0890aae1b7b523ee81e1175a
Damien Vesper <ldamienvesper@gmail.com>
2021-02-13 13:02:33 -0600

    lint

--- src/commands/vote.js
+++ src/commands/vote.js
@@ -1,7 +1,5 @@
 const Discord = require(`discord.js`);
-const User = require(`../models/user.model`);
 const { config } = require(`../index.js`);
-const { emojis } = require(`../config/emojis`);
 
 module.exports = {
     name: `vote`,
@@ -12,9 +10,6 @@ module.exports = {
 };
 
 module.exports.run = async (client, message, args) => {
-    const m = `${message.author} »`;
-    const dbUser = await User.findOne({ discordID: message.author.id });
-
     const embed = new Discord.RichEmbed()
         .setColor(0xffa500)
         .setAuthor(`Vote for Minecord`)
