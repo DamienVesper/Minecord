5341e8deeec62667d724d75aef17eaf2295ac557
Damien Vesper <ldamienvesper@gmail.com>
2021-04-23 14:52:08 -0500

    update packages, general cleanup

--- src/index.js
+++ src/index.js
@@ -2,24 +2,23 @@ const Discord = require(`discord.js`);
 require(`dotenv`).config();
 
 /* Client Config */
-const config = require(`./config/config`);
+const config = require(`./config/config.js`);
+
 const client = new Discord.Client({
     // disableEveryone: true,
     fetchAllMembers: true,
     sync: true
 });
 
-const mongoDB = require(`mongodb`);
 const mongoose = require(`mongoose`);
-
-mongoDB.connect(config.db.uri, config.db.uriParams).then(() => console.log(`Succesfully connected to MongoDB Atlas.`)).catch(err => console.error(err));
 mongoose.connect(config.db.uri, config.db.uriParams).then(() => console.log(`Succesfully connected Mongoose to MongoDB.`)).catch(err => console.error(err));
 
 module.exports = {
-    config: config,
-    client: client
+    config,
+    client
 };
-const User = require(`./models/user.model`);
+
+const User = require(`./models/user.model.js`);
 
 /* Client Events */
 client.on(`ready`, async () => {
@@ -55,18 +54,15 @@ const refreshActivity = async () => {
 client.on(`message`, async message => {
     const m = `${message.author} » `;
 
-    if (message.channel.id === `625357023315230764` || (message.guild && message.guild.id === `625357023315230760`)) return;
-
-    /* Botception & Message Handling */
+    // Message handling.
     if (message.author.bot || message.channel.type === `dm`) return;
     if (message.content.slice(0, config.prefix.length).toString().toLowerCase() !== config.prefix) return;
-    //  if(!message.content.toLowerCase().startsWith(config.prefix)) return;
 
-    /* Get Commands & Arguments */
+    // Argument handler.
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
-    /* Validate Commands */
+    // Command validation.
     const cmd = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
 
     if (!cmd || command === ``) return;
@@ -76,14 +72,7 @@ client.on(`message`, async message => {
         const dbUser = await User.findOne({ discordID: message.author.id });
         if (!dbUser && command !== `start` && command !== `help`) return message.channel.send(`${m} You don't have an account yet!\n Do \`${config.prefix}start\` to begin your adventure!`);
 
-        if (dbUser && command !== `start`) {
-            if (dbUser.banned) return message.channel.send(`${m} You're banned, fool! <:GetBannedLol:653692630093266949>`);
-            if (message.guild && message.guild.id === `625357023315230760`) {
-                const addRole = roleID => { message.member.addRole(message.guild.roles.get(roleID)); };
-                const roles = [`625451047736836116`, `625451072152141825`, `625451095791108106`, `625451121976147969`, `625451142033309723`];
-                addRole(roles[dbUser.equipped.pickaxe]);
-            }
-        }
+        if (dbUser && command !== `start`) if (dbUser.banned) return message.channel.send(`${m} You're banned, fool! <:GetBannedLol:653692630093266949>`);
 
         if (dbUser && dbUser.cooldowns[cmd.name] !== undefined) {
             if (((new Date() - new Date(dbUser.cooldowns[cmd.name])) / 1000) < cmd.cooldown) return message.channel.send(`${m}You cannot ${cmd.name} for another \`${(((cmd.cooldown * 1000) + (new Date(dbUser.cooldowns[cmd.name]) - new Date())) / 1000).toFixed(1)}\` seconds.`);
@@ -94,12 +83,13 @@ client.on(`message`, async message => {
         }
 
         try {
-            // console.log(`${message.author.tag} ran command ${command} in ${message.guild.name} [${message.guild.id}] on shard ${client.shard.id}.`);
             console.log(`${message.author.tag} ran command ${command} in ${message.guild ? `${message.guild.name} [${message.guild.id}].` : ``}`);
             cmd.run(client, message, args);
         }
-        catch (err) { console.log(`There was an error executing command ${command} by ${message.author.tag}.`);
-            message.channel.send(err); }
+        catch (err) {
+            console.log(`There was an error executing command ${command} by ${message.author.tag}.`);
+            message.channel.send(err);
+        }
     }
 });
 
