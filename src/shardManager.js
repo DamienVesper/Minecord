2399b03d9cf10155278b368707f7c18c1ed400a8
Damien Vesper <ldamienvesper@gmail.com>
2021-04-23 14:57:14 -0500

    dont use shardmanager

--- src/shardManager.js
+++
@@ -1,7 +0,0 @@
-require(`dotenv`).config();
-
-const { ShardingManager } = require(`discord.js`);
-const shardManager = new ShardingManager(`./src/index.js`, { token: process.env.DISCORD_BOT_TOKEN });
-
-shardManager.spawn(6);
-shardManager.on(`launch`, shard => console.log(`Launched Shard: ${shard.id}.`));
